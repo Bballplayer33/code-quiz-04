fab01c3b5cbe534daf14cfeab9186d735f712740
Earl Adams <earladams2000@gmail.com>
2022-01-27 11:53:34 -0500

    update css

--- script.js
+++ script.js
@@ -1,91 +1,185 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+//Create functions
+function buildQuiz(){
+    const output = [];
 
-	function showQuestions(questions, quizContainer){
-		// code will go here
-        var output = [];
-        var answers;
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+            const answers = [];
 
-        for(var i=0; i<questions.length; 1++){
-            answers = [];
+            for(letter in currentQuestion.answers){
 
-            for(letter in question[i].answers){
                 answers.push(
-                        `<label>
-                          <input type="radio" name="question${questionNumber}" value="${letter}">
-                          ${letter} :
-                          ${questions.answers[letter]}
-                        </label>`);
+                  `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                   </label>`
+                );
             }
+
             output.push(
-                `<div class="question"> ${questions.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
-              );
-
-
-
+            `<div class="slide">
+                <div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>
+            </div>`
+            );
         }
-	}
+    );
 
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
+    quizContainer.innerHTML = output.join('');
+}
 
-	// show the questions
-	showQuestions(questions, quizContainer);
+function showResults(){
+    const answersContainers = quizContainer.querySelectorAll('.answers');
 
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-  var myQuestions = [
-      {
-      question: "Commonly used data types DO NOT include:",
-      answers: {
-          1: "Strings",
-          2: "Booleans",
-          3: "Alerts",
-          4: "Numbers"},
-          correctAnswer: "Alerts"
-  },
-  {
-    question: "The condition in an if / else statment is enclosed within ______.",
-    answers: {
-        1: "Quotes",
-        2: "Curly Brackets",
-        3: "Parenthisis",
-        4: "Square Brackets",
-    },
-    correctAnswer: "Curly Brackets"
-},
-{
-    question: "Arrays in JavaScript can be used to store _____.",
-    answers: {
-        1: "Numbers and Strings",
-        2: "Other Arrays",
-        3: "Booleans",
-        4: "All of the Above"
-    },
-    correctAnswer: "All of the Above"
-},
-{
-    question: "String values must be enclosed within ______ when being assigned to variables.",
-    answers: {
-        1: "Commas",
-        2: "Curly Brackets",
-        3: "Quotas",
-        4: "Parenthisis"
-    },
-    correctAnswer: "Quotas"
-},
-{
-    question: "A very useful tool used during development an debugging for printing content to the debugger is?",
-    answers: {
-        1: "JavaScript",
-        2: "Terminal/bash",
-        3: "for Loops",
-        4: "Console Log"
-    },
-    correctAnswer: "Console Log"
-}];
+    let numCorrect = 0;
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+        const answersContainer = answersContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answersContainer.querySelector(selector) || {}).value;
+
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+
+            answersContainers[questionNumber].style.color = 'lightgreen';
+        }
+
+        else{
+            answersContainers[questionNumber].style.color = 'red';
+        }
+
+        
+
+
+
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+
+    if(currentSlide === 0) {
+        previousButton.style.display = 'none';
+    }
+    else{
+        previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else {
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
 
 }
+
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1)
+}
+
+//store references in HTML (Variables)
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+// Questions
+const myQuestions = [
+    {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: {
+        1: "The <head> section",
+        2: "The <body> section",
+        3: "Both the <head> and the <body> sections",
+        4: "In the .css file"},
+        correctAnswer: "2"
+},
+{
+  question: "How do you create a function in JavaScript?",
+  answers: {
+      1: "function = myFunction()",
+      2: "function:myFunction()",
+      3: "function myFunction()",
+      4: "function + myFunction()",
+  },
+  correctAnswer: "1"
+},
+{
+  question: "How to write an IF statement in JavaScript?",
+  answers: {
+      1: "if i === 5 then",
+      2: "if(i=== 5)",
+      3: "if i = 5",
+      4: "if - = 5 then"
+  },
+  correctAnswer: "2"
+},
+{
+  question: "How do you declare a JavaScript variable?",
+  answers: {
+      1: "var + carName",
+      2: "v carName",
+      3: "variable carName",
+      4: "Var carName"
+  },
+  correctAnswer: "4"
+},
+{
+  question: "Which operator is used to assign a value to a variable?",
+  answers: {
+      1: "*",
+      2: "-",
+      3: "=",
+      4: "x"
+  },
+  correctAnswer: "3"
+}];
+
+buildQuiz();
+
+
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+const slides = document.querySelectorAll(".slide");
+const startTime = document.getElementById("startClock");
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
+
+submitButton.addEventListener('click', showResults);
+previousButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+startTime.addEventListener('click', setInterval);
+    
+  
+    var count = 10;
+    var interval = setInterval(function(){
+      document.getElementById('count').innerHTML=count;
+      count--;
+        if (count === -2){
+        clearInterval(interval);
+        document.getElementById('count').innerHTML='Done';
+}
+    }, 1000);
+
+    localStorage.setItem("answerContainers", JSON.stringify(answersContainers));
+    renderMessage();
+
+
+    function renderMessage() {
+        var lastScore = JSON.parse(localStorage.getItem("answerContainers"))
+        document.querySelector(".high").textContent = `${numCorrect} out of ${myQuestions.length}`;
+    }
